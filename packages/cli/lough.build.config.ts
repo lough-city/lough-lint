3b41194c2aae09748ff42d0cf957d80eb136ec04
City <city@nanzc.com>
2024-02-27 21:55:59 +0800

    fix: build commands

--- packages/cli/lough.build.config.ts
+++ packages/cli/lough.build.config.ts
@@ -3,5 +3,5 @@ import { defineConfig } from '@lough/build-cli';
 export default defineConfig({
   external: ['@lough/npm-operate', '@logically/coding-model', 'chalk', 'commander', 'execa', 'inquirer', 'ora'],
   style: false,
-  input: ['src/bin.ts', 'src/index.ts']
+  input: ['src/index.ts', 'src/bin.ts', 'src/commands']
 });
